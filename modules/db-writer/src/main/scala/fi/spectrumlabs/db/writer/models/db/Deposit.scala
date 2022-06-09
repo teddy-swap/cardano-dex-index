e5dcd08a377ad19406775781abb870126ad673c0
t.gusev <gusev.t.s1@gmail.com>
2022-06-09 22:52:10 +0300

    interop

--- modules/db-writer/src/main/scala/fi/spectrumlabs/db/writer/models/db/Deposit.scala
+++ modules/db-writer/src/main/scala/fi/spectrumlabs/db/writer/models/db/Deposit.scala
@@ -33,10 +33,10 @@ object Deposit {
             op.depositCfg.depositPoolId,
             op.depositCfg.depositPair._1.value._1.asName,
             op.depositCfg.depositPair._2.value._1.asName,
-            "", //todo
+            op.lqReward.value._1.asName,
             op.depositCfg.depositPair._1.value._2,
             op.depositCfg.depositPair._2.value._2,
-            Amount(0), //todo
+            op.lqReward.value._2,
             op.depositCfg.depositExFee,
             op.depositCfg.depositRewardPkh,
             op.depositCfg.depositRewardSPkh,
--- modules/db-writer/src/main/scala/fi/spectrumlabs/db/writer/models/streaming/ExecutedDeposit.scala
+++ modules/db-writer/src/main/scala/fi/spectrumlabs/db/writer/models/streaming/ExecutedDeposit.scala
@@ -2,12 +2,13 @@ package fi.spectrumlabs.db.writer.models.streaming
 
 import derevo.circe.magnolia.{decoder, encoder}
 import derevo.derive
-import fi.spectrumlabs.db.writer.models.orders.Deposit
+import fi.spectrumlabs.db.writer.models.orders.{AssetEntry, Deposit}
 import tofu.logging.derivation.loggable
 
 @derive(decoder, encoder, loggable)
-final case class ExecutedDeposit( //todo add actual lq reward, lqName
+final case class ExecutedDeposit(
   depositCfg: Deposit,
+  lqReward: AssetEntry,
   depositOrderInputId: String,
   depositUserOutputId: String,
   currPool: String,
